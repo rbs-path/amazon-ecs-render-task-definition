42492097e935ca5f079a882616f8e15e04032dbd
Zsolt Fejer <zsolt.fejer@path.co.uk>
2022-01-27 20:16:13 +0000

    Enable Newrelic Sidecar

--- index.js
+++ index.js
@@ -1,81 +1,108 @@
-const path = require('path');
-const core = require('@actions/core');
-const tmp = require('tmp');
-const fs = require('fs');
-const aws = require('aws-sdk');
+const path = require("path");
+const core = require("@actions/core");
+const tmp = require("tmp");
+const fs = require("fs");
+const aws = require("aws-sdk");
 
 function mergeContainerDefinition(defaults, patch) {
   const { environment: envDefaults } = defaults;
   const { environment: envPatch, ...patchRest } = patch;
-  const { acc: environment } = (envPatch || []).concat(envDefaults || []).reduce(({ acc, seen }, e) => {
-    if (seen[e.name]) return { acc, seen };
-    seen[e.name] = 1;
-    acc.push(e)
-    return { acc, seen };
-  }, { acc: [], seen: {} });
+  const { acc: environment } = (envPatch || [])
+    .concat(envDefaults || [])
+    .reduce(
+      ({ acc, seen }, e) => {
+        if (seen[e.name]) return { acc, seen };
+        seen[e.name] = 1;
+        acc.push(e);
+        return { acc, seen };
+      },
+      { acc: [], seen: {} }
+    );
   return { ...defaults, ...patchRest, environment };
 }
 
 async function run() {
   try {
     const ecs = new aws.ECS({
-      customUserAgent: 'amazon-ecs-render-task-definition-for-github-actions'
+      customUserAgent: "amazon-ecs-render-task-definition-for-github-actions",
     });
 
     // Get inputs
-    const taskDefinitionFile = core.getInput('task-definition-patch', { required: true });
-    const service = core.getInput('service', { required: true });
-    const clusterName = core.getInput('cluster', { required: true });
-    const containerName = core.getInput('container-name', { required: true });
-    const image = core.getInput('image', { required: true });
-    const includeTags = core.getInput('include_tags', { required: false }) !== "false" ? ["TAGS"] : undefined;
+    const taskDefinitionFile = core.getInput("task-definition-patch", {
+      required: true,
+    });
+    const service = core.getInput("service", { required: true });
+    const clusterName = core.getInput("cluster", { required: true });
+    const containerName = core.getInput("container-name", { required: true });
+    const image = core.getInput("image", { required: true });
+    const includeTags =
+      core.getInput("include_tags", { required: false }) !== "false"
+        ? ["TAGS"]
+        : undefined;
 
     // Parse the task definition
-    const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
-      taskDefinitionFile :
-      path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
+    const taskDefPath = path.isAbsolute(taskDefinitionFile)
+      ? taskDefinitionFile
+      : path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
     if (!fs.existsSync(taskDefPath)) {
-      throw new Error(`Task definition file does not exist: ${taskDefinitionFile}`);
+      throw new Error(
+        `Task definition file does not exist: ${taskDefinitionFile}`
+      );
     }
     const taskDefPatch = require(taskDefPath);
 
     // Download the task definition
-    const describeResponse = await ecs.describeServices({
-      services: [service],
-      cluster: clusterName
-    }).promise();
+    const describeResponse = await ecs
+      .describeServices({
+        services: [service],
+        cluster: clusterName,
+      })
+      .promise();
     if (describeResponse.failures && describeResponse.failures.length > 0) {
       const failure = describeResponse.failures[0];
       throw new Error(`${failure.arn} is ${failure.reason}`);
     }
 
     const serviceResponse = describeResponse.services[0];
-    if (serviceResponse.status != 'ACTIVE') {
+    if (serviceResponse.status != "ACTIVE") {
       throw new Error(`Service is ${serviceResponse.status}`);
     }
-    const taskDefArn = serviceResponse.taskDefinition
+    const taskDefArn = serviceResponse.taskDefinition;
 
-    core.debug('Downloading the task definition');
+    core.debug("Downloading the task definition");
     let describeTaskResponse;
     try {
-      describeTaskResponse = await ecs.describeTaskDefinition({ taskDefinition: taskDefArn, include: includeTags }).promise();
+      describeTaskResponse = await ecs
+        .describeTaskDefinition({
+          taskDefinition: taskDefArn,
+          include: includeTags,
+        })
+        .promise();
     } catch (error) {
-      core.setFailed("Failed to download task definition in ECS: " + error.message);
+      core.setFailed(
+        "Failed to download task definition in ECS: " + error.message
+      );
       core.debug("Task definition name: " + taskDefArn);
-      throw (error);
+      throw error;
     }
 
-    const taskDef = describeTaskResponse.taskDefinition;
+    let taskDef = describeTaskResponse.taskDefinition;
     if (includeTags) {
       tags = describeTaskResponse.tags;
     }
-    const findContDef = taskDef.containerDefinitions.findIndex(x => x.name === containerName);
+    const findContDef = taskDef.containerDefinitions.findIndex(
+      (x) => x.name === containerName
+    );
 
     const newContainerDefinition = mergeContainerDefinition(
-      taskDef.containerDefinitions[findContDef], { ...taskDefPatch, image });
+      taskDef.containerDefinitions[findContDef],
+      { ...taskDefPatch, image }
+    );
+    taskDef.containerDefinitions.splice(findContDef, 1);
+    taskDef.containerDefinitions.push(newContainerDefinition);
 
     var newTaskDef = {
-      containerDefinitions: [newContainerDefinition],
+      containerDefinitions: taskDef.containerDefinitions,
       family: taskDef.family,
       taskRoleArn: taskDef.taskRoleArn,
       executionRoleArn: taskDef.executionRoleArn,
@@ -84,8 +111,8 @@ async function run() {
       cpu: taskDef.cpu,
       requiresCompatibilities: taskDef.requiresCompatibilities,
       volumes: taskDef.volumes,
-      placementConstraints: taskDef.placementConstraints
-    }
+      placementConstraints: taskDef.placementConstraints,
+    };
     if (includeTags) {
       newTaskDef.tags = tags;
     }
@@ -93,16 +120,15 @@ async function run() {
     // Write out a new task definition file
     var updatedTaskDefFile = tmp.fileSync({
       tmpdir: process.env.RUNNER_TEMP,
-      prefix: 'task-definition-',
-      postfix: '.json',
+      prefix: "task-definition-",
+      postfix: ".json",
       keep: true,
-      discardDescriptor: true
+      discardDescriptor: true,
     });
     const newtaskDefPatch = JSON.stringify(newTaskDef, null, 2);
     fs.writeFileSync(updatedTaskDefFile.name, newtaskDefPatch);
-    core.setOutput('task-definition', updatedTaskDefFile.name);
-  }
-  catch (error) {
+    core.setOutput("task-definition", updatedTaskDefFile.name);
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
